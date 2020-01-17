9441b80b2597b055fc30958c16f0df347f4b51e7
Kovah <mail@kovah.de>
2020-01-17 10:56:09 +0100

    Fix validation for uuid generator tool

--- app/tools/generate/uuid.rb
+++ app/tools/generate/uuid.rb
@@ -2,8 +2,7 @@ class Generate::Uuid < Tool
   attr_accessor :amount
 
   validates_presence_of :amount
-  validates_numericality_of :amount
-  validates_length_of :amount, minimum: 1, maximum: 50
+  validates_numericality_of :amount, only_integer: true, greater_than: 0, less_than_or_equal_to: 50
 
   def run
     # Generate a new array with @amount entries which are all initialized with the SecureRandom.uuid method
